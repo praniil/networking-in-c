aa31d993e044735b22d0c5658b5b2023c932cbf6
praniil <pparajuli15@gmail.com>
2025-09-17 17:14:02 +0545

    feat: bind the socket with the server address

--- udp_client_server/server.c
+++ udp_client_server/server.c
@@ -7,6 +7,7 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #define MAXLINE 1024
+#define PORT 8080
 
 int main() {
     int sockfd;
@@ -26,5 +27,15 @@ int main() {
     memset(&servaddr, 0, sizeof(servaddr));
     memset(&cliaddr, 0, sizeof(cliaddr));
 
+    //filling the server information
+    servaddr.sin_family = AF_INET; //IPv4
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
+
+    //bind the socket with the server addresss
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
     return 0;
 }
